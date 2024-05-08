e93e2aa1f2ec54fb880f7f4faba7a66a47a06212
Stanislav Tarasenko <148527948+fieryybird@users.noreply.github.com>
2024-05-08 14:29:32 +0200

    Update car-rental-administration.sql

--- DB Administration/car-rental-administration.sql
+++ DB Administration/car-rental-administration.sql
@@ -1,6 +1,8 @@
--- Create roles
-CREATE ROLE manager_role;
+-- A multi-branch organization's database security model based on employee roles and locations.
 
+-- Definition of employees + city-specific roles.
+
+CREATE ROLE manager_role;
 CREATE ROLE accountant_role;
 CREATE ROLE regular_employee_role;
 
@@ -10,13 +12,16 @@ CREATE ROLE chicago_role;
 CREATE ROLE houston_role;
 CREATE ROLE seattle_role;
 
--- Assign Grant to roles
+-- Assign permissions to employee roles.
+
 GRANT ALL ON employees, rentals, cars, car_types, customers, agreements, branches TO manager_role;
 GRANT ALL ON rentals, employees, payments, agreements TO accountant_role;
 GRANT SELECT, UPDATE, INSERT ON rentals TO regular_employee_role;
 GRANT SELECT ON cars, customers, branches TO regular_employee_role;
 
--- Create policy on employees by branches
+-- Create policies that gives only access to data related to specific branch.
+-- For "employees" table:
+
 CREATE POLICY ny_employee_policy ON employees 
     FOR ALL TO ny_role
     USING(branch_id = (SELECT branch_id FROM branches WHERE city LIKE 'New York'));
@@ -37,7 +42,8 @@ CREATE POLICY seattle_employee_policy ON employees
     FOR ALL TO seattle_role
     USING(branch_id = (SELECT branch_id FROM branches WHERE city LIKE 'Seattle'));
 
--- Create policy on cars by branches
+-- For "cars" table: 
+
 CREATE POLICY ny_car_policy ON cars
     FOR ALL TO ny_role
     USING(branch_id = (SELECT branch_id FROM branches WHERE city LIKE 'New York'));
@@ -64,39 +70,35 @@ ALTER TABLE cars ENABLE ROW LEVEL SECURITY;
 ALTER TABLE cars FORCE ROW LEVEL SECURITY;
 
 
--- Create manager users
-CREATE USER j_doe WITH PASSWORD 'supersecret'; -- NY manager
-CREATE USER m_johnson WITH PASSWORD 'supersecret'; -- LA manager
+-- Create users.
+
+CREATE USER j_doe WITH PASSWORD 'supersecret';           -- NY manager
+CREATE USER m_johnson WITH PASSWORD 'supersecret';       -- LA manager
 CREATE USER c_williams WITH PASSWORD 'supersecret'; -- Chicago manager
-CREATE USER d_brown WITH PASSWORD 'supersecret'; -- Houston manager
+CREATE USER d_brown WITH PASSWORD 'supersecret';    -- Houston manager
 CREATE USER m_martinez WITH PASSWORD 'supersecret'; -- Seattle manager
 
--- Create accountant user
-CREATE USER l_pacioli WITH PASSWORD 'strongpassword';
+CREATE USER l_pacioli WITH PASSWORD 'strongpassword';    -- Accountant
 
--- Create regular employee users
+-- regular employees 
 
--- NY employees
-CREATE USER j_smith WITH PASSWORD 'password';
-CREATE USER d_wilson WITH PASSWORD 'password';
+CREATE USER j_smith WITH PASSWORD 'password';         -- NY 
+CREATE USER d_wilson WITH PASSWORD 'password';        -- NY 
 
--- LA employees
-CREATE USER a_richardson WITH PASSWORD 'password';
-CREATE USER m_green WITH PASSWORD 'password';
+CREATE USER a_richardson WITH PASSWORD 'password';    -- LA 
+CREATE USER m_green WITH PASSWORD 'password';         -- LA 
 
--- Chicago employees
-CREATE USER a_jones WITH PASSWORD 'password';
-CREATE USER m_sanchez WITH PASSWORD 'password';
+CREATE USER a_jones WITH PASSWORD 'password';    -- Chicago
+CREATE USER m_sanchez WITH PASSWORD 'password';  -- Chicago
 
--- Houston employees
-CREATE USER s_miller WITH PASSWORD 'password';
-CREATE USER e_rivera WITH PASSWORD 'password';
+CREATE USER s_miller WITH PASSWORD 'password';   -- Houston  
+CREATE USER e_rivera WITH PASSWORD 'password';   -- Houston  
 
--- Seattle employees
-CREATE USER o_taylor WITH PASSWORD 'password';
-CREATE USER e_kelly WITH PASSWORD 'password';
+CREATE USER o_taylor WITH PASSWORD 'password';   -- Seattle 
+CREATE USER e_kelly WITH PASSWORD 'password';    -- Seattle 
 
--- Grant by roles
+-- Grant permissions by roles.
+
 GRANT manager_role, ny_role TO j_doe;
 GRANT manager_role, la_role TO m_johnson;
 GRANT manager_role, chicago_role TO c_williams;
